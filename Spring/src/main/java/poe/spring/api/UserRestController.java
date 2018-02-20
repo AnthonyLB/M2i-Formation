881818e990fd78a12bf679ce051a2aba2e62cfb3
AnthonyLB <Formation@192.168.200.89>
2018-02-20 11:26:34 +0100

    #ALB - Push project

---
+++ Spring/src/main/java/poe/spring/api/UserRestController.java
@@ -0,0 +1,31 @@
+package poe.spring.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import poe.spring.domain.User;
+import poe.spring.service.UserManagerService;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserRestController {
+
+	@Autowired
+	UserManagerService userManagerService;
+
+	@GetMapping 
+	public String hello(){
+		return "hello";
+	}
+	
+	@PostMapping
+	public User save(@RequestBody User user) {
+		User savedUser = userManagerService.signup(user.getLogin(), user.getPassword());
+		System.out.println("user id: " + savedUser);
+		return savedUser;
+	}
+}
