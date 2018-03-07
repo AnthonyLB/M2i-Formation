055f7b520e58184063cfd41b79590acb4e5ac332
AnthonyLB <anthony.lebeom@gmail.com>
2018-03-07 16:05:20 +0100

    #ALB

---
+++ Programmation_Client/src/fr/com/test/TestInterface.java
@@ -0,0 +1,12 @@
+package fr.com.test;
+
+
+public interface TestInterface {
+
+	public static void printTest() {
+		System.out.println("bob");
+		float p = 2.0f;
+	}
+	
+	public abstract void print2Test();
+}
